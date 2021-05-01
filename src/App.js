d420554b3ab0b7876501c2bb5fd80fc17bfb6430
STNyame <sentwalidors@hotmail.com>
2021-05-01 18:37:35 +0200

    skeleton resource manager

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.scss";
+import Scoreboard from "./components/Scoreboard/Scoreboard";
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <Scoreboard />
+      </main>
+    </div>
+  );
+}
+
+export default App;
